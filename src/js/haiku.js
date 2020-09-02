e44ba709ed7ee4e99c0d42933dd02bbabc4ec54e
AJ Markow <alexanderjmarkow@gmail.com>
2020-09-02 13:41:29 -0700

    Add class and a few functions to the haiku.js file. Add links in main.js to haiku logic.

---
+++ src/js/haiku.js
@@ -0,0 +1,66 @@
+class haiku {
+  constructor(lineArray1, lineArray2, lineArray3) {
+    this.lineArray1 = [];
+    this.lineArray2 = [];
+    this.lineArray3 = [];
+    this.allArrays = allArrays,
+  }
+
+  lineToArray(lineInputVariable,whichlineParameter){
+    for (const index in whichlineParameter) {
+    let splitword = lineInputVariable.split(' ')
+    whichlineParameter.push(splitword)
+    }
+    return whichlineParameter
+  }
+
+  let param1 = "Mary had a little lamb"
+  let param2 = testlinearray [];
+  console.log(lineToArray(param1,param2))
+  console.log(param2);
+
+  wordSyllableCount() {
+
+  }
+
+  lineSyllableCount() {
+    
+  }
+};
+
+function lineToArray() {
+
+let lineArray1 = ["Mary had a little lamb"];
+let brokenArray1 = [];
+
+for (i=0; i < lineArray1.length; i++) {
+  
+}
+
+}
+
+
+
+// ---Schema for:
+
+// -----haiku class
+
+// -----haiku object
+
+// -------lineArray1
+
+// -------lineArray2
+
+// -------lineArray3
+
+// -------boolean (is it a haiku)
+
+// -------syllable count for haiku object total()
+
+// ---functions within the class
+
+// -----wordSyllableCount
+
+// -----lineSyllableCount
+
+// -----lineToArray
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import haiku from "./src/js/haiku.js";
+
+$("form#some-form").submit(function (event) {
+  const line1Input = $("input#some-input").val();
+  const line2Input = $("input#some-input").val();
+  const line3Input = $("input#some-input").val();
+
+  event.preventDefault();
+});
